b8ea7ead9c954f96d37b3427607a0b4154801c86
Thorben Werner <werner@ismll.de>
2022-06-13 13:57:55 +0200

    DenseNet.py

--- reimplementations/DenseNet.py
+++ reimplementations/DenseNet.py
@@ -1,4 +1,4 @@
-import torch
+import torch, sys
 from tqdm import tqdm
 from sklearn.metrics import f1_score
 from torch.utils.data import DataLoader, TensorDataset
@@ -7,6 +7,10 @@ from torch.optim import SGD
 from torchvision import transforms
 from torchvision.models.densenet import DenseNet
 from core.Data import load_cifar10_pytorch
+
+sys.path.append("../core")
+sys.path.append("../rl_core")
+print(F"updated path is {sys.path}")
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
