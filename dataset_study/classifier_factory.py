8b8ed0f4fc8981320e4fcf1979047c7aa7f63854
Thorben Werner <werner@ismll.de>
2022-09-28 10:26:31 +0200

    agent copy

--- dataset_study/classifier_factory.py
+++ dataset_study/classifier_factory.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
 
-def create_classifier(x_train)->nn.Module:
+def create_classifier(x_train, y_train)->nn.Module:
     # this should build and return a model based on the training data (# features, # classes, etc)
     pass
