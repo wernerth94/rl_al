18483ae7fc9d5e0a292b37f0cff43e7cc9fcdbc4
Thorben Werner <werner@ismll.de>
2022-09-28 15:47:45 +0200

    cluster fix

--- core/helper_functions.py
+++ core/helper_functions.py
@@ -1,10 +1,9 @@
 import sys
 # path additions for the cluster
-sys.path.append("core")
 sys.path.append("evaluation")
 sys.path.append("config")
 sys.path.append("rl_core")
-sys.path.append("reimplementations")
+sys.path.append("../reimplementations")
 sys.path.append("dataset_study")
 
 import torch
